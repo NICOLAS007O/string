4588540b4d764fec3b9b194088604d6616a46fd4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 21:46:32 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Declaração de variáveis
+string nomeDigitado;
+string sobrenomeDigitado;
+// string nomeMaiusculas;
+string nomeLeet;
+
+Console.Clear();
+
+Console.WriteLine("--- Dados Pessoais ---");
+
+Console.Write("     Nome: ");
+nomeDigitado = Console.ReadLine()!.ToUpper(); // Atribuição
+
+nomeLeet = nomeDigitado
+            .Replace("A", "4")
+            .Replace("I", "1")
+            .Replace("E", "3");
+
+// nomeMaiusculas = nomeDigitado.ToUpper();
+
+Console.Write("Sobrenome: ");
+sobrenomeDigitado = Console.ReadLine()!;
+
+Console.WriteLine($"Boa noite, {nomeLeet} {sobrenomeDigitado.ToUpper()}!"); // Interpolação
+
+Console.Write("Pressione uma tecla para continuar...");
+Console.ReadKey(true);
